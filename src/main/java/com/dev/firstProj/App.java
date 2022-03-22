7a2e070c5b144ae0f0057e0d83debe5116fd6503
devsigi <devsigi@yahoo.com>
2022-03-22 20:03:09 +0000

    initial commit

---
+++ src/main/java/com/dev/firstProj/App.java
@@ -0,0 +1,13 @@
+package com.dev.firstProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Bro!" );
+    }
+}
